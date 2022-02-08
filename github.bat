2bf454fd35d7e7a903e41fa03e24e5f43f5051ed
svn12 <svn12@gmail.com>
2022-02-09 06:55:42 +0800

    'link'

--- github.bat
+++ github.bat
@@ -3,7 +3,7 @@ rem git config --global --unset user.email
 Rem 進入專案資料夾，單獨設定每個repo 使用者名稱/郵箱
 git config user.email "svn12@gmail.com"
 git config user.name "svn12"
-git pull
+git pull origin master
 git add -A .
 git commit -m %1
 git push origin master
